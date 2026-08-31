1612c1cb3f9406262f42627dc89823577c0a46db
schmidtweston-ship-it <schmidt.weston@gmail.com>
2026-08-31 05:22:01 +0000

    Sit Here: sw.js

---
+++ sw.js
@@ -0,0 +1,46 @@
+/* Sit Here. Same-origin only. Cache name is the version. */
+const CACHE = 'sit-here-v1';
+const SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './favicon.svg',
+  './low.wav',
+  './soft.wav',
+  './robots.txt'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(SHELL)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+  if (url.origin !== self.location.origin) return;
+  event.respondWith(
+    caches.match(req).then((hit) => {
+      if (hit) return hit;
+      return fetch(req).then((res) => {
+        const copy = res.clone();
+        if (res.ok) {
+          caches.open(CACHE).then((cache) => cache.put(req, copy)).catch(() => {});
+        }
+        return res;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
